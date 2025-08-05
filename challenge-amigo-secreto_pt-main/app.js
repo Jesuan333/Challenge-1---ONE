aeef7e824fcbd6cd55d86485a4919f0b4aa1c3e7
jesuan <jesuan.study@gmail.com>
2025-08-05 18:42:48 -0300

    Sua mensagem de commit aqui

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function addAmigos(){
+    add = document.getElementById('amigo').value; 
+    amigos.push(add);
+    limparCampo();
+    console.log(amigos);
+
+    if (add == ""){
+        alert('Insira um nome, por favor.')
+        amigos.pop("")
+    }
+
+}
+
+function exibirAmigos(){
+    exibeAmigo = document.getElementById('listaAmigos')
+
+
+}
+
+function sortearAmigo(){
+
+    númeroDoAmigo = parseInt(Math.random()*amigos.length);
+    console.log(númeroDoAmigo); 
+    console.log(amigos[númeroDoAmigo]);
+    
+}
+
+// Função para limpar o campo de preenchimento dos nomes
+function limparCampo() {
+    amg = document.querySelector('input');
+    amg.value = '';
+}
