050ee87cfdfddd4506e01cf7252585b07731c577
jesuan <jesuan.study@gmail.com>
2025-08-06 08:27:54 -0300

    Falta listas e resultado HTML

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -6,17 +6,21 @@ function addAmigos(){
     amigos.push(add);
     limparCampo();
     console.log(amigos);
-
+    
     if (add == ""){
         alert('Insira um nome, por favor.')
         amigos.pop("")
     }
 }
 
-function exibirAmigos(){
-    exibeAmigo = document.getElementById('listaAmigos')
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    
+}
 
-
+function exibirlista() {
+    exibirTextoNaTela(amigos);
 }
 
 function sortearAmigo(){
@@ -25,10 +29,19 @@ function sortearAmigo(){
     console.log(númeroDoAmigo); 
     console.log(amigos[númeroDoAmigo]);
     
+        limpaLista();
+
+
+    
 }
 
 // Função para limpar o campo de preenchimento dos nomes
 function limparCampo() {
     amg = document.querySelector('input');
     amg.value = '';
+}
+
+// Função para limpar o array dos amigos 
+function limpaLista(){
+    amigos.length = 0;
 }
